4e388a61f62579db2e737ab41d01cefb714ec8d7
Javid Memmedkhanli <145150258+Javid0800@users.noreply.github.com>
2025-04-19 21:36:00 +0400

    Eolymp 8933

---
+++ Loop/8933.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cin>>n;
+    for(i=n;i>=0;i--)
+    {
+        cout<<i<<" "<<"sek"<<endl;
+    }
+    
+    return 0;
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(n;n>=0;n--)
+    {
+        cout<<n<<" "<<"sek"<<endl;
+    }
+
+    return 0;
+}
+*/
