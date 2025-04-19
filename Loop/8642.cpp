9e38286cf5365e00a6eb6dd3c7fea2a9ed134bfd
Javid Memmedkhanli <145150258+Javid0800@users.noreply.github.com>
2025-04-19 23:01:42 +0400

    Eolymp 8642

--- Loop/8642.cpp
+++ Loop/8642.cpp
@@ -16,3 +16,33 @@ int main()
     }
     return 0;
 }
+
+// veya
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a,b,i,i1,arm,req;
+    cin>>a>>b;
+
+    for(i=a; i<=b; i++)
+    {
+        i1=i;
+        arm=0;
+        while(0<i1)
+        {
+            req=i1%10;
+            arm+=pow(req,4);
+            i1/=10;
+        }
+        if(arm==i)
+        {
+            cout<<i<<" ";
+        }
+    }
+
+    return 0;
+}
